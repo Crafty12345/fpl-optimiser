693cbec22773029dda07f267b340c662dfa1f772
Crafty12345 <hunterdylan482@gmail.com>
2025-10-24 18:19:47 +0800

    Updated predictions

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-CURRENT_DATE = "19-10-2025"
+CURRENT_DATE = "24-10-2025"
 # CURRENT_GAMEWEEK refers to the week before the week shown where it says 'Gameweek ... - Deadline' in the 'Transfers' tab
-CURRENT_GAMEWEEK = 7
+CURRENT_GAMEWEEK = 8
 MAX_ITERS = 300
 CURRENT_SEASON=26 # End year of current season
 MAX_GAMEWEEKS=34
