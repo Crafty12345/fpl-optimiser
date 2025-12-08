Fixed an issue where the wrong index was being used

modules/lin_team_predicter.py
@@ -32,15 +32,12 @@ class LinearTeamPredicter(TeamSolver):
 		if(self.verbose):
 			print("[DEBUG]: Done reading data files! Calculating linear regression...")
 
-		nameColIndex = [i for (i, col) in enumerate(self.latestData.columns) if col == "name"][0]
-		scoreColIndex = [i for (i, col) in enumerate(self.latestData.columns) if col == "score"][0]
-
 		for playerId in self.latestData.index:
 			y = np.ndarray(shape=len(self.allData), dtype=np.float32)
-			playerName = self.latestData.iat[playerId, nameColIndex]
+			playerName = self.latestData.at[playerId, "name"]
 			for (i, dataFile) in enumerate(self.allData):
 				if (playerId in dataFile.index):
-					toAppend = dataFile.iat[playerId, scoreColIndex]
+					toAppend = dataFile.at[playerId, "score"]
 				else:
 					toAppend = 0.0
 				#toAppend = dataFile.loc[dataFile["name"]==player, "score"]
@@ -52,7 +49,12 @@ class LinearTeamPredicter(TeamSolver):
 				# else:
 				# 	toAppend = 0.0
 
-				y[i] = (toAppend)
+				try:
+					y[i] = toAppend
+				except ValueError as err:
+					print(f"toAppend={toAppend}")
+					print(f"player:{dataFile.iloc[playerId]}")
+					raise err
 			x = np.arange(self.sampleSize).reshape((-1, 1))
 			model = LinearRegression().fit(x, y)
 			self.playerModelDict[playerName] = model
@@ -61,7 +63,7 @@ class LinearTeamPredicter(TeamSolver):
 			predictedWeightedScore = self.predictPlayer(xToPredict, playerName)
 
 			#print("latestData.index=", self.latestData.loc[self.latestData["name"] == player, "score"].index)
-			self.latestData.iat[playerId, scoreColIndex] = predictedWeightedScore.item()
+			self.latestData.at[playerId, "score"] = predictedWeightedScore.item()
 
 		if(self.verbose):
 			print("Done calculating linear regression!")

modules/team_predicter.py
@@ -17,6 +17,7 @@ class TeamPredicter(TeamSolver):
     
     # TODO: Optimise this method
     def concatWeeks(self, pCallback = None) -> pd.DataFrame:
+        # TODO: Add dtype parameter to DataFrame creation
         tempDf = pd.DataFrame(columns=self.allCols)
         toConcat: np.array[pd.DataFrame] = np.ndarray(len(self.allData)+1, dtype=object)
         toConcat[0] = tempDf
@@ -47,7 +48,6 @@ class TeamPredicter(TeamSolver):
         currentStatuses = set(pDatum["status"].apply(lambda x: "status_" + x))
         self.allDummyColumns = self.allDummyColumns.union(currentStatuses)
 
-    @line_profiler.profile
     def setDummies(self, pToDummy: pd.DataFrame) -> pd.DataFrame:
         result = pd.get_dummies(pToDummy, columns=self.toDummyColumns)
         colsToAdd = np.ndarray(len(self.allDummyColumns), dtype=object)
@@ -78,6 +78,7 @@ class TeamPredicter(TeamSolver):
             result["id"] = result["id"].astype("category")
         return result
 
+    @line_profiler.profile
     def fixDataTypes(self, pDf: pd.DataFrame) -> pd.DataFrame:
         pDf["points_this_week"] = pDf["points_this_week"].astype(np.float64)
         pDf["gameweek"] = pDf["gameweek"].astype(np.uint16)

modules/team_solver.py
@@ -67,6 +67,7 @@ class TeamSolver(ABC):
 			for (currentGameweek, playerData) in tempDict.items():
 				currentGameweek = int(currentGameweek)
 				currentData = pd.DataFrame.from_records(playerData)
+				currentData = currentData.set_index("id", drop=False)
 				currentData["gameweek"] = currentData["gameweek"].astype(np.uint16)
 				currentData["season"] = currentData["season"].astype(np.uint16)
 				self.precalcScores(currentData, currentGameweek, season)
@@ -89,6 +90,7 @@ class TeamSolver(ABC):
 					result = datum
 		return result
 	
+	# TODO: replace .loc with .at
 	def getOpposingTeam(self, pTeam: str, pFixtureDf: pd.DataFrame) -> str:
 		result = pFixtureDf.loc[pFixtureDf["home_team"]==pTeam]["away_team"]
 		if len(result) == 0:
@@ -96,15 +98,16 @@ class TeamSolver(ABC):
 		if len(result) == 0:
 			return "UNK"
 		else:
-			return result.values[0]
+			return result.item()
 	
 	def train(self):
 		self.default_players = dict()
-		self.latestData = self.latestData.set_index(self.latestData["id"])
+		if "id" in self.latestData.columns:
+			self.latestData = self.latestData.set_index(self.latestData["id"])
+			assert (self.latestData["id"].values == self.latestData.index.values).all()
 		
 		self.latestData.loc[((self.latestData["status"] == "i") | (self.latestData["status"] == "s")), "score"] = 0.0
 
-		assert (self.latestData["id"].values == self.latestData.index.values).all()
 		goalkeepers = self.latestData.loc[self.latestData["position"]=="GKP"]
 		defenders = self.latestData.loc[self.latestData["position"]=="DEF"]
 		forward = self.latestData.loc[self.latestData["position"]=="FWD"]